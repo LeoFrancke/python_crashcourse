c642c9dc46b96367fea95974637711e4c8c2d548
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-07-31 06:20:07 -0300

    chore: refresh python fundamentals #3

---
+++ 04_lists_part_II__loops_slices/4.99__exercises2.py
@@ -0,0 +1,30 @@
+pizzas = ['mussarela', 'four cheese']
+
+for pizza in pizzas:
+    print(pizza)
+
+print('--')
+
+
+# numerical lists
+
+for i in range(0, 11):
+    print(i)
+
+
+
+numbers = list(range(1, 6))
+print(numbers)
+
+
+odd_numbers = list(range(1, 12, 2))
+print(odd_numbers)
+
+
+squares = []
+for i in range(1, 11):
+    squares.append(i ** 2)
+
+print(squares)
+
+
