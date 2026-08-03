d575c29ac20c227f5703394d452f83d73b27f61c
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-08-03 20:42:42 -0300

    chore: refresh python fundamentals #5

--- 05_if_statements/03_multiple_conditions.py
+++ 05_if_statements/03_multiple_conditions.py
@@ -1,4 +1,13 @@
 # using AND / OR
 
 # Truth table needs to make logical sense here.
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+# intuitive for me, no need to memorize.
 
