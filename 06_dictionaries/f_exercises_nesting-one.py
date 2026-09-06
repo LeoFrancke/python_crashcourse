c889929d174c5976b958f8415fe1bf1c78901f0e
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-09-06 14:20:30 -0300

    study: dictionary nesting exercise

---
+++ 06_dictionaries/f_exercises_nesting-one.py
@@ -0,0 +1,26 @@
+
+person_1 = {
+        'name': 'Linus',
+        'lastname': 'Torvalds',
+        'age': 56,
+        'city': 'Helsinki',
+        }
+
+person_2 = {
+        'name': 'Richard',
+        'lastname': 'Feynman',
+        'age': 69,
+        'city': 'Pasadena',
+        }
+
+people = []
+people.append(person_1)
+people.append(person_2)
+
+
+for person in people:
+    full_name = (person['name'] + " " + person['lastname']).title()
+    print(f"\nFull name: {full_name}")
+    print(f"Age: {person['age']}")
+    print(f"Lived in {person['city']}")
+
