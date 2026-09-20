6c224282eb42ebc2b48422b0d9c8695e27cceaa4
Leandro Francke <219414135+leofrancke@users.noreply.github.com>
2026-09-19 21:33:00 -0300

    study(functions): exercises 9-11

---
+++ 08_functions/h_exercises__IX.py
@@ -0,0 +1,18 @@
+# 8.9 Messages
+
+def show_messages(list_of_msgs):
+    """Display each text message in the list."""
+    print()
+    for msg in list_of_msgs:
+        print(msg)
+
+
+short_messages = [
+    'hello world python',
+    'short text message',
+    'python crash course is a great book',
+]
+
+show_messages(short_messages)
+
+
---
+++ 08_functions/i_exercises__X.py
@@ -0,0 +1,21 @@
+# 8.10 Sending Messages
+
+def send_messages(list_of_msgs):
+    """Send each text message in the list, moving it to a new list."""
+    while list_of_msgs:
+        current_msg = list_of_msgs.pop()
+        print(current_msg)
+        sent_messages.append(current_msg)
+
+
+short_messages = [
+    'hello world python',
+    'short text message',
+    'python crash course is a great book',
+]
+sent_messages: list[str] = []
+
+send_messages(short_messages)
+print(short_messages)
+print(sent_messages)
+
